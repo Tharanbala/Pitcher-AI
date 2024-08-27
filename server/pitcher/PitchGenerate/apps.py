0ee63aa912fc908593298898a6dd2fa2f3033339
ShobyG <shobysekar@gmail.com>
2024-08-26 22:46:16 -0700

    Adding getPitch file to the pitchGenerate app

--- server/pitcher/PitchGenerate/apps.py
+++ server/pitcher/PitchGenerate/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PitchgenerateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PitchGenerate'
+    name = 'pitchGenerate'
